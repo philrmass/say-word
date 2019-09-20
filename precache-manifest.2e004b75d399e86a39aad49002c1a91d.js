e223caf309f2f1f1a0350a7ac45fbed9675db09e
Phil Mass <philrmass@gmail.com>
2019-09-19 22:48:45 -0700

    Updates

---
+++ precache-manifest.2e004b75d399e86a39aad49002c1a91d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77926d60791b2e0797531d7cf12a6bf5",
+    "url": "/say-word/index.html"
+  },
+  {
+    "revision": "b749979dd656e624700f",
+    "url": "/say-word/static/css/main.0abc1396.chunk.css"
+  },
+  {
+    "revision": "a5b4182c08755c772163",
+    "url": "/say-word/static/js/2.7e7ca635.chunk.js"
+  },
+  {
+    "revision": "b749979dd656e624700f",
+    "url": "/say-word/static/js/main.5096e3d9.chunk.js"
+  },
+  {
+    "revision": "7c0c10f4a29fe050cc55",
+    "url": "/say-word/static/js/runtime~main.4abc4a4f.js"
+  }
+]);
