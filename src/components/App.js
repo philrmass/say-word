4473d77517816a31107094b3da095fffd361cc36
Phil Mass <philrmass@gmail.com>
2019-09-19 22:38:06 -0700

    add logos and move app

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [results, setResults] = useState([]);
+
+  useEffect(() => {
+    const sr = new window.webkitSpeechRecognition();
+    sr.onresult = (e) => {
+      const result = e.results[e.results.length - 1][0];
+      setResults(results => [...results, result]);
+    }
+    sr.continuous = true;
+    sr.start();
+  }, []);
+
+  return (
+    <div>
+      <div className='title'>Results</div>
+      <ul>
+        { results.map((result) => (
+          <li key={result.confidence}>
+            <span>{result.transcript}</span>
+            <span className='confidence'>{(100 * result.confidence).toFixed(1)}%</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
